ba4331fe1cca9efa9b745ffdfa6ebbe519b3a731
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-12 01:15:21 +0100

    filter

--- index.php
+++ index.php
@@ -22,10 +22,27 @@
     <div class="container d-flex justify-content-center align-items-center">
         <div class="col-3">
             <form>
-                <div class="input-group mb-3">
+                <div class="input-group mb-3 d-flex">
                     <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" name="length">
-                    <button class="btn btn-outline-primary" type="submit" id="button-addon2"  >Genera</button>
                 </div>
+                <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="lettere">
+                        <label class="form-check-label" for="flexSwitchCheckDefault">Lettere</label>
+                </div>
+                <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="numeri">
+                        <label class="form-check-label" for="flexSwitchCheckDefault">Numeri</label>
+                </div>
+                <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="simboli">
+                        <label class="form-check-label" for="flexSwitchCheckDefault">Simboli</label>
+                </div>
+                <div class="input-group mb-3 d-flex">
+                    <button class="btn btn-outline-primary col-6" type="submit" id="button-addon2"  >Genera</button>
+                    <button class="btn btn-outline-secondary col-6" type="reset" id="button-addon2"  >Annulla</button>
+                </div>
+
+
             </form>
         </div>
     </div>
